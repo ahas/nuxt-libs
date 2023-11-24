98f37fa28ffa81091b319f5701b6e159cfd8e00c
ahas <dev@ahas.io>
2023-11-24 17:16:17 +0900

    feat(event-bus): Add new composable onEvent

--- libs/event-bus/composables.ts
+++ libs/event-bus/composables.ts
@@ -1,7 +1,16 @@
-import { EventBus } from "./event-bus";
+import { EventBus, type EventHandler, type EventTopic } from "./event-bus";
+import { onUnmounted } from "vue";
 
 const _eventBus = new EventBus();
 
 export function useEventBus() {
   return _eventBus;
+}
+
+export function onEvent(topic: EventTopic, handler: EventHandler) {
+  _eventBus.on(topic, handler);
+
+  onUnmounted(() => {
+    _eventBus.off(topic, handler);
+  });
 }
